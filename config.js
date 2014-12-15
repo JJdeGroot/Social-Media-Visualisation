55f93417ebcca156bdc5f423513592384e978e40
Liam Magee <liam.magee@gmail.com>
2014-12-15 14:50:31 +1100

    Need to add config for heroku deploy

---
+++ config.js
@@ -0,0 +1,23 @@
+
+var Credentials = {
+	twitter: {
+	    consumer_key:         'TWcLhdxx5uTWrqK8o7ftgoiwI',
+	    consumer_secret:      'NLvdb05yfmXpQMbyVFiYOCZJhBLrxOlgwhE3pvEDrT92cVRkRz',
+	    access_token:         '2925104532-tUvVnjeZF6edb3ivCferZoDeTA91UWRfVM6p2eg',
+	    access_token_secret:  'LaBLYek6Zu9rSlo2DlzJhTJJFvHYQQxc9VcW78QNYC9U7'
+	},
+
+	instagram: { 
+		client_id: '80e6e5d398ab41948fb653a0719bf6cd',
+	    client_secret: 'e176503effaa4173ae719fd8566b001a' 
+	},
+
+	tumblr: {
+	    consumer_key: 'Z1erJQh0TadyLCaQY6NrpmvqBOUGpUs0RL0Q6LfVMgkvUmcjfE',
+	    consumer_secret: 'OA1Ivz06n7OV3RfNw8ldOEY2KaoJhj9qbA7pMWtnT5xPBHs4r7',
+	    token: 'jWPD1vszVOjavlx5IJJc14pmOETJgGygY1GiUNSkzQO1CEsz6U',
+	    token_secret: 'SnS0mE2GAeKYvTyopTC17dLlJUKBvSp4OHv0Z7uNcSWsite1kz'
+	}
+}
+
+module.exports = Credentials
