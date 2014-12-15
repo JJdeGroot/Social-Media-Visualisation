3eced4dfca9accbd07a6ecfa7703e73f371b5ade
Liam Magee <liam.magee@gmail.com>
2014-12-15 15:02:44 +1100

    Set port to env by default

--- app.js
+++ app.js
@@ -74,5 +74,5 @@ app.get('/api/tumblr/:q', function(req, res) {
 
 
 // Listen to port 3000
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 console.log('Your app is now running at: http://127.0.0.1:3000/');
